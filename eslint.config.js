0b90796dbc4373ad7e6fc8fd22e63a041d10c8fd
JonasA3 <berghwind@gmail.com>
2025-04-19 18:54:30 +0200

    Update dependencies and lint config

---
+++ eslint.config.js
@@ -0,0 +1,43 @@
+import js from '@eslint/js';
+import ts from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import astro from 'eslint-plugin-astro';
+import astroParser from 'astro-eslint-parser';
+
+export default [
+  {
+    ignores: ['**/.astro/*.d.ts'],
+  },
+  js.configs.recommended,
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: './tsconfig.json',
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': ts,
+    },
+    rules: {
+      ...ts.configs.recommended.rules,
+    },
+  },
+  {
+    files: ['**/*.astro'],
+    plugins: {
+      '@astrojs': astro,
+    },
+    languageOptions: {
+      parser: astroParser,
+      parserOptions: {
+        parser: tsParser,
+      },
+    },
+    rules: {
+      ...astro.configs.recommended.rules,
+    },
+  },
+];
