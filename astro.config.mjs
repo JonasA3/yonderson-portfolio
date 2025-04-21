66ac8d3408841f046e48e95e69e23529db13c9e2
JonasA3 <berghwind@gmail.com>
2025-04-21 11:45:56 +0200

    Remove astro config from .ignore and add

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+
+export default defineConfig({
+  integrations: [
+    tailwind({
+      configFile: './tailwind.config.js',
+      applyBaseStyles: true,
+      nesting: true,
+    }),
+  ],
+});
