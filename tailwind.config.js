52842273eb6f1b9afe8f30d3a6f2940d46581116
JonasA3 <berghwind@gmail.com>
2025-04-21 12:00:30 +0200

    Remove duplicate block to test which one works

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
+/* eslint-env node */
+/* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
+module.exports = {
+  content: ['./src/**/*.{astro,html,js,jsx,ts,tsx}', './public/**/*.html'],
   theme: {
     extend: {
       fontFamily: {
@@ -81,22 +83,6 @@ export default {
         50: '0.5',
         75: '0.75',
         100: '1',
-      },
-    },
-  },
-  plugins: [],
-};
-
-/* eslint-env node */
-/* eslint-disable no-undef */
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./src/**/*.{astro,html,js,jsx,ts,tsx}', './public/**/*.html'],
-  theme: {
-    extend: {
-      fontFamily: {
-        logo: ['"Bebas Neue"', 'sans-serif'],
-        body: ['Quicksand', 'sans-serif'],
       },
     },
   },
