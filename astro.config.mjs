b20d0ba771dfed45986acfe486816d7d7dafeb22
JonasA3 <berghwind@gmail.com>
2025-05-22 14:14:25 +0200

    Add @astro/netlify for production SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,16 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import netlify from '@astrojs/netlify';
 
 import svelte from '@astrojs/svelte';
 
 export default defineConfig({
+  output: 'server',
+  adapter: netlify({
+    edge: false,
+    split: false,
+  }),
+  middleware: true,
   integrations: [
     tailwind({
       configFile: './tailwind.config.js',
